a51cd236549fe471bca5596a2f4924a600597434
Gustavo Soares <gustavosoaresdev10@gmail.com>
2022-07-19 09:19:57 -0300

    controllers e rotas de produtos finalizados

--- back/src/controllers/ProdutoController.ts
+++ back/src/controllers/ProdutoController.ts
@@ -1,3 +1,78 @@
-export class ProdutoController{
+import { Request, Response } from 'express';
+import { CreateProductModel } from '../models/produtoModel';
 
+type Produto = {
+  _id: string;
+  nome: string;
+  estoque: string;
+  preco: number;
+  quantidade: number;
+};
+
+class ProdutoController{
+  async store(req: Request, res: Response){
+    const body = req.body;
+
+    if (!body.nome) {
+      return res.status(400).json({ error: `Você deve enviar um nome` });
+    }
+
+    if (!body.estoque) {
+      return res.status(400).json({ error: `Você deve enviar uma descricao`});
+    }
+
+    if (!body.preco) {
+      return res.status(400).json({ error: `Você deve enviar um preço` });
+    }
+    
+    const novoProduto = await CreateProductModel.create(body)
+
+    return res.status(200).json({msg: 'Produto criado', produto: novoProduto})
+  }
+
+  async index(req: Request, res: Response){
+    const produtos = await CreateProductModel.find();
+
+    if (!produtos) return res.status(404).json('Não há produtos no estoque');
+
+    return res.status(200).json(produtos);
+  } 
+
+  async showEstoque(req: Request, res: Response){
+    try{
+      const { descricao } = req.body;
+      const produtos = await CreateProductModel.find({descricao: descricao});
+
+      return res.status(200).json(produtos)
+    }catch(e){
+      console.log(e)
+    } 
+  }
+
+  async update(req: Request, res: Response){
+    const params = req.params;
+    const body = req.body;
+
+    if(!params.id) return res.status(400).json('Você deve colocar o id')
+
+    const produtoAtualizado = await CreateProductModel.findByIdAndUpdate({ _id: params.id }, body)
+
+    if(!produtoAtualizado) return res.status(404).json('Id não encontrado')
+
+    return res.status(200).json({msg: 'Produto atualizado',produto: produtoAtualizado});
+  }
+
+  async delete(req: Request, res: Response){
+    const params = req.params;
+
+    if(!params.id) return res.status(400).json('Você deve colocar o id')
+
+    const produtoDeletado = await CreateProductModel.findByIdAndRemove({ _id: params.id })
+
+    if(!produtoDeletado) return res.status(404).json('Id não encontrado')
+
+    return res.status(200).json({msg: 'Produto deletado', produto: produtoDeletado})
+  }
 }
+
+export const produtoController = new ProdutoController();
--- back/src/routes/produtoRoutes.ts
+++ back/src/routes/produtoRoutes.ts
@@ -1,2 +1,15 @@
-import { Router as routes } from "express";
-import { ProdutoController } from "../controllers/ProdutoController";
+import { produtoController } from "../controllers/ProdutoController";
+import { estoqueController } from "../controllers/EstoqueController";
+import { Router } from "express";
+
+const routesProduto = Router();
+
+routesProduto.post('/', produtoController.store)
+routesProduto.get('/', produtoController.index)
+routesProduto.delete('/:id', produtoController.delete)
+routesProduto.put('/:id', produtoController.update)
+
+routesProduto.get('/show', estoqueController.showEstoque)
+routesProduto.get('/delete', estoqueController.deleteEstoque)
+
+export { routesProduto }
